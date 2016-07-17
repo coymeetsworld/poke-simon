ecc726634f7def565c531d7aa6eb3aaab08d57e6
Coy Sanders <coymeetsworld@gmail.com>
2016-07-16 23:12:17 -0700

    Simon: Adding click listeners for the buttons on the board.

--- simon.js
+++ simon.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+
+
+  $("#greenButton").click(function() {
+    console.log("Green button pressed.");
+  });
+  $("#redButton").click(function() {
+    console.log("Red button pressed.");
+  });
+  $("#blueButton").click(function() {
+    console.log("Blue button pressed.");
+  });
+  $("#yellowButton").click(function() {
+    console.log("Yellow button pressed.");
+  });
+  $("#strictButton").click(function() {
+    console.log("Strict button pressed.");
+  });
+  $("#startButton").click(function() {
+    console.log("Start button pressed.");
+  });
 });
