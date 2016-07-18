d5fcfb0c18f2d3117a98b20b1423b5abbc172ed1
Coy Sanders <coymeetsworld@gmail.com>
2016-07-18 01:23:01 -0700

    Simon: Preventing images from being dragged or show blue highlight when pressed. Also added feature to play the start of the sound even if a sound is already going (kills it) in order to make button press more responsive.

--- js/simon.js
+++ js/simon.js
@@ -7,21 +7,32 @@ $(document).ready(function() {
 
   $("#sevenSegDisplay").sevenSeg({ digits: 2, value: '--' });
 
+
+  function playSound(sound) {
+    sound.pause(); // in case sound is playing when button is clicked again.
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  $('.pokemonImage').on('dragstart', function(event) { event.preventDefault(); });
+  $('.pokemonImage').on('highlight', function(event) { event.preventDefault(); });
+
+
   $("#greenButton").click(function() {
     console.log("Green button pressed.");
-    bulbasaurSound.play();
+    playSound(bulbasaurSound);
   });
   $("#redButton").click(function() {
     console.log("Red button pressed.");
-    charmanderSound.play();
+    playSound(charmanderSound);
   });
   $("#blueButton").click(function() {
     console.log("Blue button pressed.");
-    squirtleSound.play();
+    playSound(squirtleSound);
   });
   $("#yellowButton").click(function() {
     console.log("Yellow button pressed.");
-    pikachuSound.play();
+    playSound(pikachuSound);
   });
   $("#strictButton").click(function() {
     console.log("Strict button pressed.");
