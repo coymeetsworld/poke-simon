e524d425b7c00e944e300daff20a76959227bce9
Coy Sanders <coymeetsworld@gmail.com>
2016-07-18 00:17:57 -0700

    Simon: Adding Pokemon sounds and included them in the JavaScript file. When the buttons are pushed the appropriate sound is played.

--- js/simon.js
+++ js/simon.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
 
+  var bulbasaurSound = new Audio('media/bulbasaur.wav');
+  var charmanderSound = new Audio('media/charmander.wav');
+  var squirtleSound = new Audio('media/squirtle.wav');
+  var pikachuSound = new Audio('media/pikachu.wav');
+
   $("#sevenSegDisplay").sevenSeg({ digits: 2, value: '--' });
+
   $("#greenButton").click(function() {
     console.log("Green button pressed.");
+    bulbasaurSound.play();
   });
   $("#redButton").click(function() {
     console.log("Red button pressed.");
+    charmanderSound.play();
   });
   $("#blueButton").click(function() {
     console.log("Blue button pressed.");
+    squirtleSound.play();
   });
   $("#yellowButton").click(function() {
     console.log("Yellow button pressed.");
+    pikachuSound.play();
   });
   $("#strictButton").click(function() {
     console.log("Strict button pressed.");
